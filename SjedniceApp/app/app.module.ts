63451ea2a6f4937fe007d0f69f12a18169f9148d
Faris Džafić <Faris Dafi>
2016-11-12 15:26:10 +0100

    Confirm dialog

--- SjedniceApp/app/app.module.ts
+++ SjedniceApp/app/app.module.ts
@@ -4,6 +4,7 @@ import { SjedniceRoutingModule } from './app.routing';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms'; // potrebno za binding na formama
+import { ConfirmationPopoverModule } from 'angular-confirmation-popover';
 
 import { Providers } from './app.providers';
 import { Declarations } from './app.declarations';
@@ -12,7 +13,14 @@ import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports: [BrowserModule, HttpModule, SjedniceRoutingModule, NgbModule.forRoot(), FormsModule],
+  imports: [BrowserModule,
+   HttpModule, 
+   SjedniceRoutingModule, 
+   NgbModule.forRoot(), 
+   FormsModule,
+   ConfirmationPopoverModule.forRoot({
+      confirmButtonType: 'danger' // set defaults here
+    })],
   declarations: Declarations,
   bootstrap: [AppComponent],
   providers: Providers
